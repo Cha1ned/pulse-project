67795fa8e1b7f25e017679469ca6902f8f69c110
Nick <Cha1nedddd@gmail.com>
2020-10-08 17:30:56 +0300

    All sections and media finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
 	$('.carousel__inner').slick({
 		speed: 1200,
+		adaptiveHeight: true,
 		slidesToShow: 1,
 		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
 		responsive: [
 			{
-				breakpoint: 991,
+				breakpoint: 576,
 				settings: {
-					adaptiveHeight: true,
-					dots: false,
-					arrows: true
+					dots: true,
+					arrows: false
 				}
 			},
 		]
@@ -34,4 +34,19 @@ $(document).ready(function(){
 
 	toggleSlide('.catalog-item__link');
 	toggleSlide('.catalog-item__back');
+
+	//modal
+
+	$('[data-modal=consultation]').on('click', function() {
+		$('.overlay, #consultation').fadeIn();
+	});
+	$('.modal__close').on('click', function() {
+		$('.overlay, #consultation, #order, #thanks').fadeOut();
+	})
+	$('.button_mini').each(function(i) {
+		$(this).on('click', function() {
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn();
+		})
+	})
 });
